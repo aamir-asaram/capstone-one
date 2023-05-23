2aa92f597b71f56240f6e376f806e26570282112
Aamir <aamirasaram2107@gmail.com>
2023-05-23 06:40:46 +0200

    Implement more button functionality

--- main.js
+++ main.js
@@ -40,9 +40,7 @@ const speakers = [
   },
 ];
 
-
-
-document.addEventListener('DOMContentLoaded', () => {
+function genSpeakers(loopTo) {
   const title = document.createElement('h3');
   title.textContent = 'Featured Speakers';
   const hr = document.createElement('hr');
@@ -50,29 +48,76 @@ document.addEventListener('DOMContentLoaded', () => {
   speakersSection.appendChild(title);
   speakersSection.appendChild(hr);
   const container = document.createElement('div');
-  speakers.forEach((speaker) => {
+  for (let i = 0; i < loopTo; i++) {
     container.classList.add('speaker-content');
     const speakerCard = document.createElement('div');
     speakerCard.classList.add('speaker');
     speakerCard.innerHTML = `
-      <img src="${speaker.img}" alt="${speaker.name}">
+      <img src="${speakers[i].img}" alt="${speakers[i].name}">
       <section>
-        <h3>${speaker.name}</h3>
-        <p class="subheading">${speaker.title}</p>
+        <h3>${speakers[i].name}</h3>
+        <p class="subheading">${speakers[i].title}</p>
         <hr>
-        <article>${speaker.bio}</article>
+        <article>${speakers[i].bio}</article>
       </section>
     `;
-    if (speaker.catergory === 'code') {
+    if (speakers[i].catergory === 'code') {
       speakerCard.classList.add('code');
     }
     container.appendChild(speakerCard);
-  })
-
+  }
   speakersSection.appendChild(container);
   const button = document.createElement('button');
+  if (loopTo < speakers.length) {
   button.textContent = 'MORE';
+  } else {
+  button.textContent = 'LESS';
+  }
   speakersSection.appendChild(button);
-});
+}
 
-const more = document.querySelector('#speakers button');
+if (window.innerWidth >= 768) {
+  document.addEventListener('DOMContentLoaded', genSpeakers(speakers.length));
+} else {
+  document.addEventListener('DOMContentLoaded', genSpeakers(2));
+}
+
+//generate function to add more speakers on click
+const generateSpeakers = () => {
+  const speakersSection = document.querySelector('#speakers');
+  speakersSection.innerHTML = '';
+
+  if (more.textContent === 'MORE') {
+    genSpeakers(speakers.length);
+    more = document.querySelector('#speakers button');
+    more.addEventListener('click', generateSpeakers);
+  } else {
+    genSpeakers(2);
+    more.textContent = 'MORE';
+    more = document.querySelector('#speakers button');
+    more.addEventListener('click', generateSpeakers);
+  }
+};
+
+let more = document.querySelector('#speakers button');
+more.addEventListener('click', generateSpeakers);
+
+const speaker = document.querySelector('#speakers');
+let desktop = false;
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 768 && !desktop) {
+    console.log('desktop');
+    desktop = true;
+    speaker.innerHTML = '';
+    genSpeakers(speakers.length);
+    const more = document.querySelector('#speakers button');
+    more.addEventListener('click', generateSpeakers);
+  } else if (window.innerWidth < 768 && desktop) {
+    desktop = false;
+    console.log('mobile');
+    speaker.innerHTML = '';
+    genSpeakers(2);
+    const more = document.querySelector('#speakers button');
+    more.addEventListener('click', generateSpeakers);
+  }
+});
